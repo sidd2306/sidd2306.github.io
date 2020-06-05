ec61b494dd88649bdd1567973563f4af624aec85
Siddhesh Shivgan <49452754+sidd2306@users.noreply.github.com>
2020-06-05 16:52:15 +0530

    Dark Mode

--- script.js
+++ script.js
@@ -1,10 +1,12 @@
 function changeColor(color) { 
-                document.body.style.background = color; 
+                document.body.style.background = color;
             } 
 function dark() { 
                 changeColor('black'); 
                 document.getElementById("darkm").innerHTML = "Double Click to set Light Theme!";
+                document.getElementById("desc").style.color = "white"
 }
 function light() { 
                 changeColor('white'); 
+                document.getElementById("desc").style.color = "black"
 }
